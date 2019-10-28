c2508efb92fd3571c4f7087faae86d908817676f
Shreesh Arya <shreesharya20@gmail.com>
2019-10-28 23:36:45 +0530

    Create magicball8.py

---
+++ magicball8.py
@@ -0,0 +1,5 @@
+from random import randint
+
+messages=['It is certain','It is decidedly so','Yes definitely','reply hazy tru again!','Ask again later','Concentrate and try again','My reply is no','Outlook is not good','very doubtful']
+i=randint(0,len(messages)-1)
+print(messages[i])
